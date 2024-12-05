8df791f79e29985f90a8e443921a1d5558ef4473
Rayan <154925122+Rayanlupo@users.noreply.github.com>
2024-12-05 11:30:15 +0000

    linking html to javascripts

--- src/popup.js
+++
@@ -1,8 +0,0 @@
-const parent = document.getElementById("entries");
-
-document.getElementById("newEntry").addEventListener("click", newEntry);
-
-function newEntry() {
-    var newDiv = document.createElement("div");
-    parent.appendChild(newDiv);
-}
